5237e4294d810b827c880504aeb6b7cfdde110ed
Praful360 <53175654+Praful360@users.noreply.github.com>
2021-05-07 18:34:06 +0545

    ok

---
+++ EmployeeManagementSystem/Controllers/Admin/DepartmentController.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class Department : Controller
+    {
+       
+    }
+}
---
+++ EmployeeManagementSystem/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Department()
+        {
+            return View("Department/Index");
+        }
+    }
+}
